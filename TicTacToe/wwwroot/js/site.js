8164bf61d1ac6ecfb7f7b7db32d31222c213a946
Yijun Lin <yijun@ksu.edu>
2019-11-11 17:30:06 -0600

    note

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+
+var turnElement = document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < square.length; i++) {
+    squares[i].addEventListener('click', onClick);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = currentTurn;
+        if (!checkForWin()) nextTurn();
+    }
+}
+
+function nextTurn() {
+    if (currentTurn === "X") {
+        currentTurn = "O";
+    }
+    else {
+        currentTurn = "X";
+    }
+    document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+}
+
+function checkForWin() {
+    for (var i = 0; i < 9; i += 3) {
+        if (squares[i].innerText === squares[i+1].innerText && squares[i+1].innerText === squares[i+2].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+    for (var j = 0; j < 3; j += 1) {
+        if (squares[j].innerText && aquares[j].innerText === squares[j + 3].innerText && squares[j + 3].innerText === squares[j + 6].innerText) {
+            declareWinner();
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+function declareWinner() {
+    document.getElementById("turn").innerText = "Player " + currentTurn + " wins!";
+    currentTurn = "";
+}
